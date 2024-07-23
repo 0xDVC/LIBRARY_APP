ce6ad6f0a01af477daa49be9c2e476aaeded82b2
dvc-77 <neilohene@gmail.com>
2024-07-23 00:36:11 +0000

    feat: building components

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,18 +1,24 @@
-import { Stack } from "expo-router";
+import {SplashScreen} from "expo-router";
 import {useFonts} from "expo-font";
+import {Slot} from "expo-router";
+import {useEffect} from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-    const [ fontsLoaded] = useFonts({
+    const [ fontsLoaded, error] = useFonts({
         "Roboto-Mono": require("../assets/fonts/RobotoMono.ttf")
     });
+    useEffect(() => {
+        if (error) throw error;
+        if (fontsLoaded) SplashScreen.hideAsync();
+    }, [fontsLoaded, error]);
 
     if (!fontsLoaded) {
         return null;
     }
 
     return (
-    <Stack>
-      <Stack.Screen name="index" />
-    </Stack>
+    <Slot />
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -2,8 +2,9 @@ import { Text, View } from "react-native";
 
 export default function Index() {
     return (
-        <View className="flex-1 justify-center items-center">
-            <Text className="text-2xl">Hello World</Text>
+        <View className="flex-1 bg-white justify-center items-center">
+
+            <Text className="text-4xl">Book App</Text>
         </View>
 
     );
