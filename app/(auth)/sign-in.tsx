eec5538c9962a0baf7aa0070504b0aca19dd5b9c
An0n-xen <fyfybaisieamoah6@gmail.com>
2024-08-10 19:58:13 +0000

    init setup

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,170 +1,171 @@
-import React, { useState } from 'react';
-import { View, Text, ScrollView, SafeAreaView } from 'react-native';
+import React, { useState } from "react";
+import { View, Text, ScrollView, SafeAreaView } from "react-native";
 import FormField from "@/components/FormField";
 import Button from "@/components/Button/Button";
-import { validateEmail, validatePhoneNumber } from '@/lib/utils';
+import { validateEmail, validatePhoneNumber } from "@/lib/utils";
 import FormFeedbackModal from "@/components/FormFeedbackModal";
 import { Colors } from "@/constants/Colors";
 import { Link } from "expo-router";
 import Checkbox from "@/components/Checkbox";
 import SocialLoginButton from "@/components/Button/SocialLoginButton";
-import { FontAwesome } from '@expo/vector-icons';
+import { FontAwesome } from "@expo/vector-icons";
 
 export default function SignIn() {
-    const [form, setForm] = React.useState({
-        email: "",
-        phoneNumber: "",
-    });
+  const [form, setForm] = React.useState({
+    email: "",
+    phoneNumber: "",
+  });
 
+  const [isChecked, setIsChecked] = useState(false);
 
-    const [isChecked, setIsChecked] = useState(false);
+  const handleCheckboxChange = (checked: boolean) => {
+    setIsChecked(checked);
+  };
 
-    const handleCheckboxChange = (checked: boolean) => {
-        setIsChecked(checked);
-    };
+  const [modalVisible, setModalVisible] = React.useState(false);
+  const [modalMessage, setModalMessage] = React.useState("");
+  const [modalType, setModalType] = React.useState<"success" | "error">(
+    "error"
+  );
 
-    const [modalVisible, setModalVisible] = React.useState(false);
-    const [modalMessage, setModalMessage] = React.useState('');
-    const [modalType, setModalType] = React.useState<'success' | 'error'>('error');
+  const handleChangeText = (name: string, value: string) => {
+    setForm({ ...form, [name]: value });
+  };
 
+  const showModal = (message: string, type: "success" | "error") => {
+    setModalMessage(message);
+    setModalType(type);
+    setModalVisible(true);
+  };
 
-    const handleChangeText = (name: string, value: string) => {
-        setForm({ ...form, [name]: value });
-    };
+  const handleSignIn = () => {
+    const { email, phoneNumber } = form;
+    const isEmail = email.includes("@");
 
-    const showModal = (message: string, type: 'success' | 'error') => {
-        setModalMessage(message);
-        setModalType(type);
-        setModalVisible(true);
-    };
+    const isEmailInput = isEmail && email.trim() !== "";
+    const isPhoneInput = !isEmail && phoneNumber.trim() !== "";
 
+    if (isEmailInput) {
+      if (!validateEmail(email)) {
+        showModal("Please enter a valid email address.", "error");
+        return;
+      }
+    } else if (isPhoneInput) {
+      if (!validatePhoneNumber(phoneNumber)) {
+        showModal("Please enter a valid phone number.", "error");
+        return;
+      }
+    } else {
+      showModal("Please enter a valid email address or phone number.", "error");
+      return;
+    }
 
-    const handleSignIn = () => {
-        const { email, phoneNumber } = form;
-        const isEmail = email.includes('@');
+    showModal("Sign up successful!", "success");
+  };
 
-        const isEmailInput = isEmail && email.trim() !== '';
-        const isPhoneInput = !isEmail && phoneNumber.trim() !== '';
+  const [isSubmitting, setIsSubmitting] = React.useState(false);
 
-        if (isEmailInput) {
-            if (!validateEmail(email)) {
-                showModal('Please enter a valid email address.', 'error');
-                return;
-            }
-        } else if (isPhoneInput) {
-            if (!validatePhoneNumber(phoneNumber)) {
-                showModal('Please enter a valid phone number.', 'error');
-                return;
-            }
-        } else {
-            showModal('Please enter a valid email address or phone number.', 'error');
-            return;
-        }
+  const handlePress = async () => {
+    setIsSubmitting(true);
+    handleSignIn();
+    setIsSubmitting(false);
+  };
 
-        showModal('Sign up successful!', 'success');
-    };
+  const handleGoogleLogin = () => {
+    // Handle Google login logic
+    console.log("Google login pressed");
+  };
 
-    const [isSubmitting, setIsSubmitting] = React.useState(false);
+  const handleAppleLogin = () => {
+    // Handle Apple login logic
+    console.log("Apple login pressed");
+  };
 
-    const handlePress = async () => {
-        setIsSubmitting(true);
-        handleSignIn();
-        setIsSubmitting(false);
-    };
+  return (
+    <SafeAreaView
+      className="h-full"
+      style={{ backgroundColor: Colors.light.background }}
+    >
+      <ScrollView>
+        <View className="w-full px-4 my-6 justify-center h-full">
+          <Text className="font-pbold text-center text-4xl mt-[35px]">
+            Sign In
+          </Text>
+          <Text className="font-pregular text-center mt-4">Welcome Back!</Text>
+          <FormField
+            title="Email or Phone Number"
+            icon="mail-outline"
+            placeholder=" Enter Email or Phone Number"
+            value={form.email || form.phoneNumber}
+            handleChangeText={(text) => {
+              if (text.includes("@")) {
+                handleChangeText("email", text);
+              } else {
+                handleChangeText("phoneNumber", text);
+              }
+            }}
+            otherStyles="mt-4"
+          />
 
-    const handleGoogleLogin = () => {
-        // Handle Google login logic
-        console.log('Google login pressed');
-    };
-
-    const handleAppleLogin = () => {
-        // Handle Apple login logic
-        console.log('Apple login pressed');
-    };
-
-    return (
-        <SafeAreaView className="h-full" style={{ backgroundColor: Colors.light.background }}>
-            <ScrollView>
-                <View className="w-full px-4 my-6 justify-center h-full">
-                    <Text className="font-pbold text-center text-4xl mt-[35px]">
-                        Sign In
-                    </Text>
-                    <Text className="font-pregular text-center mt-4">
-                        Welcome Back!
-                    </Text>
-                    <FormField
-                        title="Email or Phone Number"
-                        icon="mail-outline"
-                        placeholder=" Enter Email or Phone Number"
-                        value={form.email || form.phoneNumber}
-                        handleChangeText={(text) => {
-                            if (text.includes('@')) {
-                                handleChangeText('email', text);
-                            } else {
-                                handleChangeText('phoneNumber', text);
-                            }
-                        }}
-                        otherStyles="mt-4"
-                    />
-
-                    <View className="justify-start mt-4">
-                        <Checkbox
-                            text="Remember me"
-                            checked={isChecked}
-                            onChange={handleCheckboxChange} />
-                    </View>
-
-
-
-                    <View className="mt-10">
-                        <Button
-                            text="Sign In"
-                            onPress={handlePress}
-                            loading={isSubmitting}
-                            variant="primary"
-                            size="large"
-                            color={Colors.light.primary}
-                            fullWidth
-                        />
-                    </View>
-<FontAwesome />
-                    {/* Divider with "or" */}
-                    <View className="flex-row items-center mt-10">
-                        <View className="flex-1 h-px bg-gray-300" />
-                        <Text className="mx-4 text-gray-500 font-pmedium">or</Text>
-                        <View className="flex-1 h-px bg-gray-300" />
-                    </View>
-
-                    {/* Alternate login buttons */}
-                    <View className="flex-row justify-evenly mt-6">
-                        <SocialLoginButton
-                            provider="google"
-                            onPress={handleGoogleLogin}
-                            color={Colors.light.primary}
-                        />
-                        <SocialLoginButton
-                            provider="apple"
-                            onPress={handleAppleLogin}
-                            color="black"
-                        />
-                    </View>
-
-                    <View className="mt-16 justify-center items-center">
-                        <Text className="font-pregular">
-                            Don't have an account?{' '}
-                            <Text style={{ color: Colors.light.primary }}>
-                                <Link href="/sign-up" className="font-pmedium">Sign Up</Link>
-                            </Text>
-                        </Text>
-                    </View>
-                </View>
-            </ScrollView>
-            <FormFeedbackModal
-                visible={modalVisible}
-                message={modalMessage}
-                type={modalType}
-                onClose={() => setModalVisible(false)}
+          <View className="justify-start mt-4">
+            <Checkbox
+              text="Remember me"
+              checked={isChecked}
+              onChange={handleCheckboxChange}
             />
-        </SafeAreaView>
-    );
+          </View>
+
+          <View className="mt-10">
+            <Button
+              text="Sign In"
+              onPress={handlePress}
+              loading={isSubmitting}
+              variant="primary"
+              size="large"
+              color={Colors.light.primary}
+              fullWidth
+            />
+          </View>
+          <FontAwesome />
+          {/* Divider with "or" */}
+          <View className="flex-row items-center mt-10">
+            <View className="flex-1 h-px bg-gray-300" />
+            <Text className="mx-4 text-gray-500 font-pmedium">or</Text>
+            <View className="flex-1 h-px bg-gray-300" />
+          </View>
+
+          {/* Alternate login buttons */}
+          <View className="flex-row justify-evenly mt-6">
+            <SocialLoginButton
+              provider="google"
+              onPress={handleGoogleLogin}
+              color={Colors.light.primary}
+            />
+            <SocialLoginButton
+              provider="apple"
+              onPress={handleAppleLogin}
+              color="black"
+            />
+          </View>
+
+          <View className="mt-16 justify-center items-center">
+            <Text className="font-pregular">
+              Don't have an account?{" "}
+              <Text style={{ color: Colors.light.primary }}>
+                <Link href="/sign-up" className="font-pmedium">
+                  Sign Up
+                </Link>
+              </Text>
+            </Text>
+          </View>
+        </View>
+      </ScrollView>
+      <FormFeedbackModal
+        visible={modalVisible}
+        message={modalMessage}
+        type={modalType}
+        onClose={() => setModalVisible(false)}
+      />
+    </SafeAreaView>
+  );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,54 +1,56 @@
 import { SplashScreen, Stack } from "expo-router";
-import { useFonts } from "expo-font"
+import { useFonts } from "expo-font";
 import { useEffect } from "react";
 import { StatusBar } from "react-native";
 
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [fontsLoaded, error] = useFonts({
+    "SF-Pro-Regular": require("../assets/fonts/SF-Pro-Regular.ttf"),
+    "SF-Pro-Bold": require("../assets/fonts/SF-Pro-Bold.ttf"),
+    "SF-Pro-Thin-Italic": require("../assets/fonts/SF-Pro-Thin-Italic.ttf"),
+    "SF-Pro-Medium": require("../assets/fonts/SF-Pro-Regular.ttf"),
+    "SF-Pro-Light-Italic": require("../assets/fonts/SF-Pro-Light-Italic.ttf"),
+    "Poppins-Regular": require("../assets/fonts/Poppins-Regular.ttf"),
+    "Poppins-Bold": require("../assets/fonts/Poppins-Bold.ttf"),
+    "Poppins-SemiBold": require("../assets/fonts/Poppins-SemiBold.ttf"),
+    "Poppins-Medium": require("../assets/fonts/Poppins-Medium.ttf"),
+    "Poppins-Light": require("../assets/fonts/Poppins-Light.ttf"),
+    "Poppins-ExtraLight": require("../assets/fonts/Poppins-ExtraLight.ttf"),
+    "Poppins-ExtraBold": require("../assets/fonts/Poppins-ExtraBold.ttf"),
+    "Poppins-Black": require("../assets/fonts/Poppins-Black.ttf"),
+  });
 
-    const [fontsLoaded, error] = useFonts({
-        "SF-Pro-Regular": require("../assets/fonts/SF-Pro-Regular.ttf"),
-        "SF-Pro-Bold": require("../assets/fonts/SF-Pro-Bold.ttf"),
-        "SF-Pro-Thin-Italic": require("../assets/fonts/SF-Pro-Thin-Italic.ttf"),
-        "SF-Pro-Medium": require("../assets/fonts/SF-Pro-Regular.ttf"),
-        "SF-Pro-Light-Italic": require("../assets/fonts/SF-Pro-Light-Italic.ttf"),
-        "Poppins-Regular": require("../assets/fonts/Poppins-Regular.ttf"),
-        "Poppins-Bold": require("../assets/fonts/Poppins-Bold.ttf"),
-        "Poppins-SemiBold": require("../assets/fonts/Poppins-SemiBold.ttf"),
-        "Poppins-Medium": require("../assets/fonts/Poppins-Medium.ttf"),
-        "Poppins-Light": require("../assets/fonts/Poppins-Light.ttf"),
-        "Poppins-ExtraLight": require("../assets/fonts/Poppins-ExtraLight.ttf"),
-        "Poppins-ExtraBold": require("../assets/fonts/Poppins-ExtraBold.ttf"),
-        "Poppins-Black": require("../assets/fonts/Poppins-Black.ttf"),
-    });
+  useEffect(() => {
+    if (error) throw error;
 
-    useEffect(() => {
-        if (error) throw error;
+    if (fontsLoaded) SplashScreen.hideAsync();
+  }, [fontsLoaded, error]);
 
-        if (fontsLoaded) SplashScreen.hideAsync();
-    }, [fontsLoaded, error]);
+  if (!fontsLoaded && !error) return null;
 
-    if (!fontsLoaded && !error) return null;
+  return (
+    <Stack>
+      <Stack.Screen
+        name="index"
+        options={{ headerShown: false }}
+      ></Stack.Screen>
 
-    
+      <Stack.Screen
+        name="(auth)"
+        options={{ statusBarColor: "#fff", headerShown: false }}
+      ></Stack.Screen>
 
-    return (
-        <Stack>
+      <Stack.Screen
+        name="(tabs)"
+        options={{ headerShown: false }}
+      ></Stack.Screen>
 
-            <Stack.Screen name="index" options={{ headerShown: false }}>
-            </Stack.Screen>
-
-            <Stack.Screen name="(auth)" options={{ statusBarColor:'#fff',headerShown: false }}>
-            </Stack.Screen>
-
-            <Stack.Screen name="(tabs)" options={{ headerShown: false }}>
-            </Stack.Screen>
-
-            {/* <Stack.Screen name="(search)" options={{ headerShown: false }}>
-            </Stack.Screen> */}
-
-        </Stack>
-
-    );
+      <Stack.Screen
+        name="(profile)"
+        options={{ headerShown: false }}
+      ></Stack.Screen>
+    </Stack>
+  );
 }
